1193d8e137654f2f138313c493539dcc01537d89
Chickydalareast <phantrunghieu3101@gmail.com>
2024-06-21 03:08:12 +0700

    question1

--- Lab3.sql
+++ Lab3.sql
@@ -4,6 +4,11 @@ USE [FUH_COMPANY]
 GO
 
 --Trunghieu--
+ --1.	Cho biết ai đang quản lý phòng ban có tên: Phòng Nghiên cứu và phát triển. --
+ SELECT e.empSSN, e.empName, d.depNum, d.depName
+FROM tblEmployee e
+JOIN tblDepartment d ON e.empSSN = d.mgrSSN
+WHERE d.depName = N'Phòng Nghiên cứu và phát triển';
 
 --LanNhi--
 
